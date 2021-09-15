8026151d7c7147e10c921938ba6bf80b8d21bb3e
surajmoolya <moolyasuraj12@gmail.com>
2021-09-16 00:24:22 +0530

    DEMO

---
+++ src/test/java/Browsertest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+
+public class Browsertest {
+	@Parameters("browser")
+	@Test
+	public void google_search(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath+"//drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement search_box = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+		search_box.click();
+		search_box.sendKeys("vesit\n");
+		driver.close();
+		driver.quit();
+
+	}
+
+}
---
+++ src/test/java/BrowsertestTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class BrowsertestTest {
+
+  @Test
+  public void google_searchTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
